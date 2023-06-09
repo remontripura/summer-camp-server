88bd7d96bff0cacdd2ea3b65ab71efd4714725e3
Remon Tripura <remontripura045@gmail.com>
2023-06-09 21:39:08 +0600

    conditinaly dashboard menu item setup

--- index.js
+++ index.js
@@ -37,8 +37,6 @@ async function run() {
             res.send(result);
         })
 
-
-
         app.post('/users', async (req, res) => {
             const user = req.body;
             const query = {email: user.email}
@@ -48,6 +46,18 @@ async function run() {
             }
             const result = await usersCollection.insertOne(user);
             res.send(result)
+        })
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)}
+            const updateDoc = {
+                $set: {
+                  role: `admin`
+                },
+              };
+              const result = await usersCollection.updateOne(filter, updateDoc)
+              res.send(result)
         })
 
 
