faecb7adcaacb7f60335570ef62ece0c30f01e66
Remon Tripura <remontripura045@gmail.com>
2023-06-10 13:22:24 +0600

    jwt api create

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-var jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -32,18 +32,24 @@ async function run() {
         const usersCollection = client.db('wolvesDb').collection('users');
         const selectCollection = client.db('wolvesDb').collection('select');
 
+        // jwt
+        app.post('/jwt', (req, res) => {
+            const token = jwt.sign(user, env.process.ACCESS_TOKEN, { expiresIn: '1hr' })
+            res.send(token)
+        })
+
         // users api
-        app.get('/users', async(req, res) => {
+        app.get('/users', async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result);
         })
 
         app.post('/users', async (req, res) => {
             const user = req.body;
-            const query = {email: user.email}
+            const query = { email: user.email }
             const existingUser = await usersCollection.findOne(query);
-            if(existingUser){
-                return res.send({message: 'user already exists'})
+            if (existingUser) {
+                return res.send({ message: 'user already exists' })
             }
             const result = await usersCollection.insertOne(user);
             res.send(result)
@@ -51,29 +57,29 @@ async function run() {
 
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = {_id: new ObjectId(id)}
+            const filter = { _id: new ObjectId(id) }
             const updateDoc = {
                 $set: {
-                  role: `admin`
+                    role: `admin`
                 },
-              };
-              const result = await usersCollection.updateMany(filter, updateDoc)
-              res.send(result)
+            };
+            const result = await usersCollection.updateMany(filter, updateDoc)
+            res.send(result)
         })
 
         app.patch('/users/instructor/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = {_id: new ObjectId(id)}
+            const filter = { _id: new ObjectId(id) }
             const updateDoc = {
                 $set: {
-                  role: `instructor`
+                    role: `instructor`
                 },
-              };
-              const result = await usersCollection.updateMany(filter, updateDoc)
-              res.send(result)
+            };
+            const result = await usersCollection.updateMany(filter, updateDoc)
+            res.send(result)
         })
 
-       
+
 
         // find all data from class collection
         app.get('/class', async (req, res) => {
